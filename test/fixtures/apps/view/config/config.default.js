de9e9f55eaf6023b4c9f2107c72e07b62b315326
hubcarl <hubcarl@126.com>
2017-04-10 20:22:21 +0800

    style:rm error comments

--- test/fixtures/apps/view/config/config.default.js
+++ test/fixtures/apps/view/config/config.default.js
@@ -3,12 +3,7 @@ module.exports = () => {
   const config = {};
 
   config.keys = '123456';
-  /**
-   * swig View options
-   * @property {String|Object|Function} manifest 资源依赖表
-   * @property {Boolean} [cache] 是否缓存 如果是则为内存缓存
-   * @property {String} [ext] 模板后缀名
-   */
+
   config.security = {
     csp: {
       enable: true,
